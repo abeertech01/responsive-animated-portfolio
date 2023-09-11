af13d0ddc434324b45d65cf10f53238611901329
abeertech01 <abeer.technology@gmail.com>
2023-09-11 22:31:55 +0600

    navigation bar

--- app/components/button/Button.tsx
+++ app/components/button/Button.tsx
@@ -2,8 +2,37 @@
 
 import React from "react"
 
-function Button() {
-  return <div>Button</div>
+interface ButtonProps {
+  text: string
+  onClick?: () => void
+  bg?: string
+  modifier?: string
+  padding?: string
+  borderRadius?: string
+  hover?: string
+}
+
+function Button({
+  text,
+  onClick,
+  bg,
+  modifier = "primary",
+  padding,
+  borderRadius = "60px",
+  hover,
+}: ButtonProps) {
+  return (
+    <button
+      className={`button button--${modifier}`}
+      style={{
+        background: bg,
+        padding: padding || ".8rem 2rem",
+        borderRadius: borderRadius || "1rem",
+      }}
+    >
+      {text}
+    </button>
+  )
 }
 
 export default Button
--- app/components/navigation/Navigation.tsx
+++ app/components/navigation/Navigation.tsx
@@ -1,9 +1,46 @@
 "use client"
 
 import React from "react"
+import Button from "../button/Button"
+import Image from "next/image"
+import menuItems from "../../utils/menuItems"
+import Link from "next/link"
 
 function Navigation() {
-  return <div>Navigation</div>
+  return (
+    <nav className="navigation">
+      <ul className="nav-items">
+        <Image
+          src={"/logo.png"}
+          alt="logo"
+          width={45}
+          height={45}
+          style={{
+            objectFit: "contain",
+            backgroundColor: "#f2f6ff",
+            borderRadius: "50%",
+            padding: ".3rem",
+          }}
+        />
+
+        {menuItems.map((item) => {
+          return (
+            <li key={item.id} className="nav-items__item">
+              <Link href={item.url}>{item.title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+      <Button
+        text="Contact Us"
+        bg="#a855f7"
+        modifier="white"
+        borderRadius="60px"
+        padding="1.2rem 2rem"
+        hover="true"
+      />
+    </nav>
+  )
 }
 
 export default Navigation
